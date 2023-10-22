e732699a39ac7ebef595fec9ad6273b8b9661239
goob-goob <gabriel.miller.developer@gmail.com>
2023-10-22 10:10:00 -0700

    prod update

--- backend/middleware/oAuth.js
+++ backend/middleware/oAuth.js
@@ -14,7 +14,27 @@ oAuth = async (req, res, next) => {
 
     
     if (!code) {
-        // res.status(401).send("Missing authorization code");
+        const domain = 'https://id.twitch.tv/oauth2/authorize?'
+        const client_id = 'l0du0xx5ebj8ooe09vc9gidum44lsg'
+        const redirect_uri = 'https://followfolders.com/raid'
+        // const redirect_uri = 'http://localhost:3000/raid'
+        const response_type = 'code'
+        const scope = 'user:read:follows channel:manage:raids'
+        const state = ''
+
+        const response = await fetch(
+            `${domain}` + 
+            `client_id=${client_id}&` + 
+            `redirect_uri=${redirect_uri}&` +
+            `response_type=${response_type}&` +
+            `scope=${scope}`,
+            {
+                method: 'POST',
+                redirect: 'manual'
+            }
+        )
+        console.log(response)
+        res.redirect(redirect_uri)
     }
     if(keys.hasOwnProperty(code)) {
         console.log(`Code: ${code} already used. Sending ${keys.code} authorization token`)
